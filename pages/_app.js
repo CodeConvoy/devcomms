59bbfd3b872ed984754f3e517158c853dea28b8c
Cooper Saye <coopersaye@gmail.com>
2021-08-18 20:30:03 -0700

    Import favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,9 @@ export default function App(props) {
       <Head>
         <title>Devcomms</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
       </Head>
       <Component {...pageProps} />
     </>
