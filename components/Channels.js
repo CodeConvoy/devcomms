43e3bcd98cda5372d6ad56ada9050f81bce11629
Cooper Saye <coopersaye@gmail.com>
2021-08-19 12:22:52 -0700

    Implement channel selection.

---
+++ components/Channels.js
@@ -0,0 +1,40 @@
+import Channel from './Channel.js';
+
+import firebase from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
+
+import styles from '../styles/Channels.module.css';
+
+export default function Channels(props) {
+  const { group } = props;
+
+  const [currChannel, setCurrChannel] = useState(undefined);
+
+  // retrieve group channels
+  const groupsRef = firebase.firestore().collection('groups');
+  const groupRef = groupsRef.doc(group);
+  const channelsRef = groupRef.collection('channels');
+  const [channels] = useCollectionData(channelsRef, { idField: 'id' });
+
+  // return if loading
+  if (!channels) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <div className={styles.channels}>
+      {
+        channels.map(channel =>
+          <button onClick={setCurrChannel(channel.id)} key={channel.id}>
+            {channel.name}
+          </button>
+        )
+      }
+      </div>
+      {
+        currChannel &&
+        <Channel group={props.group} channel={currChannel} />
+      }
+    </div>
+  );
+}
