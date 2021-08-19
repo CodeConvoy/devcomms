fcb4271d4d82171ed7a6029ef509e18f2fb54296
Cooper Saye <coopersaye@gmail.com>
2021-08-19 11:14:36 -0700

    Create landing page.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,46 @@
+import Link from 'next/link';
+
+import firebase from 'firebase/app';
+
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
   return (
     <div>
+      <div>
+        <h1>Devcomms</h1>
+        <p>Team communication and coordination all in one place.</p>
+        <p>Devcomms is an open source, developer-focused team communication and
+        coordination tool aiming to simplify and streamline group development.
+        </p>
+        <p>Instead of having one tool for chatting and calling, one tool for
+        arranging notes, and one tool for setting goals and to-dos, you can do
+        all of that and more with Devcomms. No longer will you have to struggle
+        to coordinate across several apps.</p>
+        {
+          firebase.auth().currentUser ?
+          <button onClick={() => firebase.auth().signOut()}>Sign Out</button> :
+          <>
+            <Link href="/signin">
+              <a>Sign In</a>
+            </Link>
+            <Link href="/signup">
+              <a>Sign Up</a>
+            </Link>
+          </>
+        }
+        <p>
+          Devcomms is 100% free and open source, forever. Have an issue? Want to
+          contribute? Find our GitHub here:{' '}
+          <a
+            href="https://github.com/codeconvoy/devcomms"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Devcomms GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
