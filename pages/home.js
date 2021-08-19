5c779cec26f0e131224dfba1cc505bd68fa1e7c7
Cooper Saye <coopersaye@gmail.com>
2021-08-19 13:35:35 -0700

    Implement group creation.

--- pages/home.js
+++ pages/home.js
@@ -9,15 +9,28 @@ import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   const [currGroup, setCurrGroup] = useState(undefined);
+  const [name, setName] = useState('');
 
   // retrieve user groups
-  const uid = firebase.auth().currentUser.uid;
+  const uid = firebase.auth().currentUser?.uid;
   const groupsRef = firebase.firestore().collection('groups');
-  const groupsQuery = groupsRef.where('members', 'array-contains', uid);
+  const groupsQuery = groupsRef.where('members', 'array-contains', uid ?? 'null');
   const [groups] = useCollectionData(groupsQuery, { idField: 'id' });
 
   // return if loading
   if (!groups) return <div>Loading...</div>;
+
+  // creates new group doc in firebase
+  async function createGroup() {
+    setName('');
+    const uid = firebase.auth().currentUser.uid;
+    await groupsRef.add({
+      name: name,
+      creator: uid,
+      members: [uid],
+      created: new Date()
+    });
+  }
 
   return (
     <div className={styles.container}>
@@ -32,6 +45,18 @@ export default function Home() {
             )
           }
         </div>
+        <form onSubmit={e => {
+          e.preventDefault();
+          createGroup();
+        }}>
+          <input
+            placeholder="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required
+          />
+          <button>New Group</button>
+        </form>
         {
           currGroup &&
           <Channels group={currGroup} />
