e9250d99fb0772d749327c889fd4323b1b3afe94
Cooper Saye <coopersaye@gmail.com>
2021-08-19 11:32:53 -0700

    Improve sign in page styling.

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,10 @@
+import Link from 'next/link';
+
 import { useState } from 'react';
 import firebase from 'firebase/app';
 import getError from '../util/getError.js';
+
+import styles from '../styles/SignIn.module.css';
 
 export default function SignIn() {
   const [email, setEmail] = useState('');
@@ -20,35 +24,40 @@ export default function SignIn() {
   }
 
   return (
-    <div>
-      <h2>Sign In</h2>
-      <form onSubmit={e => {
-        e.preventDefault();
-        signIn();
-      }}>
-        <label htmlFor="signin-email">Email</label>
-        <input
-          id="signin-email"
-          placeholder="email"
-          type="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          autoComplete="email"
-          required
-        />
-        <label htmlFor="signin-password">Password</label>
-        <input
-          id="signin-password"
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          autoComplete="current-password"
-          required
-        />
-        <button>Sign In</button>
-      </form>
-      {error && <p>{error}</p>}
+    <div className={styles.container}>
+      <div className={styles.center}>
+        <h1>Sign In</h1>
+        <form onSubmit={e => {
+          e.preventDefault();
+          signIn();
+        }}>
+          <label htmlFor="signin-email">Email</label>
+          <input
+            id="signin-email"
+            placeholder="email"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            autoComplete="email"
+            required
+          />
+          <label htmlFor="signin-password">Password</label>
+          <input
+            id="signin-password"
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            autoComplete="current-password"
+            required
+          />
+          <button>Sign In</button>
+        </form>
+        {error && <p>{error}</p>}
+        <Link href="/signup">
+          <a>No account? Sign up</a>
+        </Link>
+      </div>
     </div>
   );
 }
