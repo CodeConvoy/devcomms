cc61918c20d6341e44167b41cac615db84d231eb
Cooper Saye <coopersaye@gmail.com>
2021-08-19 12:01:45 -0700

    Create home page.

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header.js';
+
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+}
