98d2fdd9d61420a7b3ef89f49c85436298c78ca5
Cooper Saye <coopersaye@gmail.com>
2021-08-21 12:08:29 -0400

    Implement create channel modal.

--- components/Channels.js
+++ components/Channels.js
@@ -1,5 +1,6 @@
 import Loading from './Loading.js';
 import Channel from './Channel.js';
+import Modal from '@material-ui/core/Modal';
 
 import firebase from 'firebase/app';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -12,12 +13,14 @@ export default function Channels(props) {
 
   const [currChannel, setCurrChannel] = useState(undefined);
   const [name, setName] = useState('');
+  const [open, setOpen] = useState(false);
 
   // retrieve group channels
   const groupsRef = firebase.firestore().collection('groups');
   const groupRef = groupsRef.doc(group);
   const channelsRef = groupRef.collection('channels');
-  const [channels] = useCollectionData(channelsRef, { idField: 'id' });
+  const channelsQuery = channelsRef.orderBy('name');
+  const [channels] = useCollectionData(channelsQuery, { idField: 'id' });
 
   // creates new channel doc in firebase
   async function createChannel() {
@@ -29,32 +32,42 @@ export default function Channels(props) {
   if (!channels) return <Loading />;
 
   return (
-    <div>
+    <>
       <div className={styles.channels}>
         {
           channels.map(channel =>
-            <button onClick={() => setCurrChannel(channel.id)} key={channel.id}>
+            <button
+              className={currChannel === channel.id ? styles.selected : undefined}
+              onClick={() => setCurrChannel(channel.id)}
+              key={channel.id}>
               {channel.name}
             </button>
           )
         }
+        <button onClick={() => setOpen(true)}>+</button>
       </div>
-      <form onSubmit={e => {
-        e.preventDefault();
-        createChannel();
-      }}>
-        <input
-          placeholder="name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          required
-        />
-        <button>New Channel</button>
-      </form>
       {
         currChannel &&
         <Channel group={props.group} channel={currChannel} />
       }
-    </div>
+      <Modal
+        className={styles.modal}
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <form onSubmit={e => {
+          e.preventDefault();
+          createChannel();
+        }}>
+          <input
+            placeholder="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required
+          />
+          <button>New Channel</button>
+        </form>
+      </Modal>
+    </>
   );
 }
