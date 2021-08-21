c9bcc1327daf06e6e4e219847f60920fa415abff
Cooper Saye <coopersaye@gmail.com>
2021-08-21 19:06:48 -0400

    Implement message deletion.

--- components/Message.js
+++ components/Message.js
@@ -1,9 +1,29 @@
+import firebase from 'firebase/app';
+
 import styles from '../styles/Message.module.css';
 
 export default function Message(props) {
-  const { text } = props;
+  const { channelRef } = props;
+  const { text, sender, id } = props.message;
+
+  const uid = firebase.auth().currentUser.uid;
+
+  // retrieve message ref
+  const messagesRef = channelRef.collection('messages');
+  const messageRef = messagesRef.doc(id);
+
+  // deletes message
+  async function deleteMessage() {
+    await messageRef.delete();
+  }
 
   return (
-    <div>{text}</div>
+    <div>
+      <span>{text}</span>
+      {
+        uid === sender &&
+        <button onClick={deleteMessage}>Delete</button>
+      }
+    </div>
   );
 }
