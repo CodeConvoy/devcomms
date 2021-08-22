9700f36ea2c467d51e6ad32e7b54f909fb2c22a2
Cooper Saye <coopersaye@gmail.com>
2021-08-22 10:21:11 -0400

    Create Widgets component.

---
+++ components/Widgets.js
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Widgets.module.css';
+
+export default function Widgets() {
+  return (
+    <div className={styles.container}>
+      widgets
+    </div>
+  );
+}
