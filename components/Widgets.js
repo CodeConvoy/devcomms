77038d9bff125f04c86f8a5dfb558f714445c155
Cooper Saye <coopersaye@gmail.com>
2021-08-27 19:52:08 -0400

    Populate widgets.

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,9 +1,15 @@
+import Todos from './Todos.js';
+import Sketch from './Sketch.js';
+
 import styles from '../styles/components/Widgets.module.css';
 
-export default function Widgets() {
+export default function Widgets(props) {
+  const { group, channel } = props;
+
   return (
     <div className={styles.container}>
-      widgets
+      <Todos group={group} channel={channel} />
+      <Sketch group={group} channel={channel} />
     </div>
   );
 }
