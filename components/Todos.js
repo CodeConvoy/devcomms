b512e2c86791fd9d78ce8720f743b9d8577d91af
Cooper Saye <coopersaye@gmail.com>
2021-08-27 19:29:33 -0400

    Create Todos component.

---
+++ components/Todos.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import styles from '../styles/components/Todos.module.css';
+
+export default function Todos(props) {
+  const { group, channel } = props;
+
+  // retrieve todos reference
+  const groupsRef = firebase.firestore().collection('groups');
+  const groupRef = groupsRef.doc(group);
+  const channelsRef = groupRef.collection('channels')
+  const channelRef = channelsRef.doc(channel);
+  const widgetsRef = channelRef.colection('widgets');
+  const todosRef = widgetsRef.doc('todos').collection('todos');
+
+  const [todos] = useCollectionData(todosRef, { idField: 'id' });
+
+  return (
+    <div>
+      {
+        todos.map(todo =>
+          <div key={todo.id}>{todo.title}</div>
+        )
+      }
+    </div>
+  );
+}
