013e4c11b8b8957f520d26ab7ce63f81eb8e8cb7
Cooper Saye <coopersaye@gmail.com>
2021-08-27 19:44:42 -0400

    Get sketch canvas context.

--- components/Sketch.js
+++ components/Sketch.js
@@ -1,7 +1,16 @@
 import styles from '../styles/components/Sketch.module.css';
+import { useEffect, useRef } from 'react';
+
+// width and height of canvas
+const width = 256;
+const height = 256;
+
+let canvas, ctx;
 
 export default function Sketch(props) {
   const { group, channel } = props;
+
+  const canvasRef = useRef();
 
   // retrieve sketch reference
   const groupsRef = firebase.firestore().collection('groups');
@@ -11,8 +20,19 @@ export default function Sketch(props) {
   const widgetsRef = channelRef.colection('widgets');
   const sketchRef = widgetsRef.doc('sketch');
 
+  // retrieve canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
+      <canvas
+        ref={canvasRef}
+        width={width}
+        height={height}
+      />
     </div>
   );
 }
