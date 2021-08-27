3d5a18d886d4080bcde752e109021cab7af302ab
Cooper Saye <coopersaye@gmail.com>
2021-08-27 19:52:15 -0400

    Implement widget toggle.

--- components/Widgets.js
+++ components/Widgets.js
@@ -6,10 +6,27 @@ import styles from '../styles/components/Widgets.module.css';
 export default function Widgets(props) {
   const { group, channel } = props;
 
+  const [showTodos, setShowTodos] = useState(true);
+  const [showSketch, setShowSketch] = useState(true);
+
   return (
     <div className={styles.container}>
-      <Todos group={group} channel={channel} />
-      <Sketch group={group} channel={channel} />
+      <label htmlFor="widgets-showtodos">Todos</label>
+      <input
+        id="widgets-showtodos"
+        type="checkbox"
+        checked={showTodos}
+        onChange={e => setShowTodos(!showTodos)}
+      />
+      <label htmlFor="widgets-showsketch">Sketch</label>
+      <input
+        id="widgets-showsketch"
+        type="checkbox"
+        checked={showSketch}
+        onChange={e => setShowSketch(!showSketch)}
+      />
+      {showTodos && <Todos group={group} channel={channel} />}
+      {showSketch && <Sketch group={group} channel={channel} />}
     </div>
   );
 }
