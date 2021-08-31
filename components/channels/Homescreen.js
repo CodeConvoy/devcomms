f38b216c5d29a972ca5682e59cafce0efd5ffdbb
Cooper Saye <coopersaye@gmail.com>
2021-08-30 22:06:44 -0700

    Map users to buttons.

--- components/channels/Homescreen.js
+++ components/channels/Homescreen.js
@@ -1,9 +1,26 @@
+import Loading from '../Loading.js';
+
+import firebase from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 import styles from '../../styles/components/channels/Homescreen.module.css';
 
 export default function Homescreen() {
+  const usersRef = firebase.firestore().collection('users');
+  const [users] = useCollectionData(usersRef, { idField: 'id' });
+
+  if (!users) return <Loading />;
+
   return (
     <div className={styles.container}>
       <div className={styles.users}>
+        {
+          users.map(user =>
+            <button key={user.id}>
+              {user.username}
+            </button>
+          )
+        }
       </div>
     </div>
   );
