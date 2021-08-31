aa883ed89a01b89a9cad490e8d0cd8ae4998c374
Cooper Saye <coopersaye@gmail.com>
2021-08-30 22:03:20 -0700

    Create Homescreen component.

---
+++ components/channels/Homescreen.js
@@ -0,0 +1,10 @@
+import styles from '../../styles/components/channels/Homescreen.module.css';
+
+export default function Homescreen() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.users}>
+      </div>
+    </div>
+  );
+}
