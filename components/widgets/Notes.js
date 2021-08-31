f651f3f972b724c04c1fd8bcce77d18ebb1c9b60
Cooper Saye <coopersaye@gmail.com>
2021-08-31 14:49:14 -0700

    Integrate notes with firebase.

--- components/widgets/Notes.js
+++ components/widgets/Notes.js
@@ -1,11 +1,15 @@
+import Loading from '../Loading.js';
 import DescriptionIcon from '@material-ui/icons/Description';
 
 import firebase from 'firebase/app';
+import { useEffect, useState } from 'react';
 
 import styles from '../../styles/components/widgets/Notes.module.css';
 
 export default function Notes(props) {
   const { group, widget } = props;
+
+  const [text, setText] = useState(undefined);
 
   // retrieve notes reference
   const groupsRef = firebase.firestore().collection('groups');
@@ -13,10 +17,31 @@ export default function Notes(props) {
   const channelsRef = groupRef.collection('channels')
   const channelRef = channelsRef.doc(widget.id);
 
+  // gets existing text from firebase
+  async function getText() {
+    const channelDoc = await channelRef.get();
+    const data = channelDoc.data();
+    setText(data.text ?? '');
+  }
+
+  // saves text to firebase
+  async function saveText() {
+    await channelRef.update({ text });
+  }
+
+  // get text on start
+  useEffect(() => {
+    getText();
+  }, []);
+
+  // return if loading
+  if (text === undefined) return <Loading />;
+
   return (
     <div className={styles.container}>
       <h1><DescriptionIcon fontSize="large" /> <span>{widget.name}</span></h1>
-      <textarea />
+      <textarea value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={saveText}>Save</button>
     </div>
   );
 }
