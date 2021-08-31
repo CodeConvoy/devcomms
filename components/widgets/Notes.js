6e510fe2efe08e11319c6024e1c9748d0cc5f765
Cooper Saye <coopersaye@gmail.com>
2021-08-31 14:44:02 -0700

    Improve notes styling.

--- components/widgets/Notes.js
+++ components/widgets/Notes.js
@@ -1,19 +1,22 @@
+import DescriptionIcon from '@material-ui/icons/Description';
+
 import firebase from 'firebase/app';
 
 import styles from '../../styles/components/widgets/Notes.module.css';
 
 export default function Notes(props) {
-  const { group, channel } = props;
+  const { group, widget } = props;
 
   // retrieve notes reference
   const groupsRef = firebase.firestore().collection('groups');
   const groupRef = groupsRef.doc(group);
   const channelsRef = groupRef.collection('channels')
-  const channelRef = channelsRef.doc(channel);
+  const channelRef = channelsRef.doc(widget.id);
 
   return (
-    <div>
-      <textarea rows={17} cols={30} />
+    <div className={styles.container}>
+      <h1><DescriptionIcon fontSize="large" /> <span>{widget.name}</span></h1>
+      <textarea />
     </div>
   );
 }
