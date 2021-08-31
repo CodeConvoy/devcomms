3dc3e8cc5288b2662ba8edd23df6625258777260
Cooper Saye <coopersaye@gmail.com>
2021-08-30 22:54:29 -0700

    Implement user selection.

--- components/channels/Homescreen.js
+++ components/channels/Homescreen.js
@@ -2,10 +2,13 @@ import Loading from '../Loading.js';
 
 import firebase from 'firebase/app';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useState } from 'react';
 
 import styles from '../../styles/components/channels/Homescreen.module.css';
 
 export default function Homescreen() {
+  const [currUser, setCurrUser] = useState(undefined);
+
   const usersRef = firebase.firestore().collection('users');
   const [users] = useCollectionData(usersRef, { idField: 'id' });
 
@@ -16,7 +19,10 @@ export default function Homescreen() {
       <div className={styles.users}>
         {
           users.map(user =>
-            <button key={user.id}>
+            <button
+              onClick={() => setCurrUser(user)}
+              key={user.id}
+            >
               {user.username}
             </button>
           )
