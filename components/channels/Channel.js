1bebb853950dff0e25a07b5c858768a89e3a2a0b
Cooper Saye <coopersaye@gmail.com>
2021-09-03 12:33:55 -0700

    Create edit channel modal.

--- components/channels/Channel.js
+++ components/channels/Channel.js
@@ -15,9 +15,45 @@ export default function Channel(props) {
     >
       {getIcon(type)}
       <span>{channel.name}</span>
-      <button className={styles.editbutton} onClick={() => {}}>
+      <button
+        className={styles.editbutton}
+        onClick={e => {
+          e.stopPropagation();
+          resetModal();
+          setModalOpen(true);
+        }}
+      >
         <SettingsIcon />
       </button>
+      <Modal
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+      >
+        <div className="modal">
+          <h1>Editing {channel.name}</h1>
+          <form onSubmit={e => {
+            e.preventDefault();
+            updateChannel();
+          }}>
+            <input
+              value={name}
+              className={`${styles.nameinput} darkinput`}
+              onChange={e => setName(e.target.value)}
+              placeholder="name"
+              required
+            />
+            <button className="iconbutton2">
+              <CheckIcon />
+            </button>
+          </form>
+          <button
+            className={`iconbutton2 ${styles.delbutton}`}
+            onClick={deleteChannel}
+          >
+            <DeleteIcon />
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
