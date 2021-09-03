f1d171e1703c9cd2573d9a6e1f217e29f7e2fc95
Cooper Saye <coopersaye@gmail.com>
2021-09-02 21:18:39 -0700

    Implement group deletion.

--- components/Group.js
+++ components/Group.js
@@ -1,4 +1,8 @@
+import Modal from '@material-ui/core/Modal';
 import SettingsIcon from '@material-ui/icons/Settings';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import { useState } from 'react';
 
 import styles from '../styles/Group.module.css';
 
@@ -7,7 +11,16 @@ import firebase from 'firebase/app';
 export default function Group(props) {
   const { group, currGroup, setCurrGroup, selectStyle } = props;
 
+  const [modalOpen, setModalOpen] = useState(false);
+
   const uid = firebase.auth().currentUser.uid;
+
+  // deletes group
+  async function deleteGroup() {
+    if (window.confirm('Really delete group?')) {
+      await firebase.firestore().collection('groups').doc(group.id).delete();
+    }
+  }
 
   return (
     <div
@@ -21,10 +34,21 @@ export default function Group(props) {
       <div>{group.name}</div>
       {
         uid === group.creator &&
-        <button onClick={e => e.stopPropagation()}>
+        <button onClick={() => setModalOpen(true)}>
           <SettingsIcon fontSize="small" />
         </button>
       }
+      <Modal
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+      >
+        <div className="modal">
+          <h1>Editing Group</h1>
+          <button className="iconbutton2" onClick={deleteGroup}>
+            <DeleteIcon />
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
