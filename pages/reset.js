c1d8eb697a42b4e456179e938879de050656c5f6
Cooper Saye <coopersaye@gmail.com>
2021-09-07 08:54:09 -0700

    Implement password reset.

--- pages/reset.js
+++ pages/reset.js
@@ -1,9 +1,29 @@
 import Router from 'next/router';
 
+import { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import getError from '../util/getError.js';
+
 import styles from '../styles/pages/Reset.module.css';
 
 export default function Reset(props) {
   const { currentUser } = props;
+
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  // sends password reset email
+  async function resetPassword() {
+    setError('');
+    setSuccess('');
+    try {
+      await firebase.auth().sendPasswordResetEmail(email);
+      setSuccess(`Email sent to ${email}`);
+    } catch (e) {
+      setError(getError(e));
+    }
+  }
 
   // listen for user auth
   useEffect(() => {
