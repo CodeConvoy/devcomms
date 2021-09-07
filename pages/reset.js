ceb54a92f96c0f7800abcad0a612c4f1f23055c8
Cooper Saye <coopersaye@gmail.com>
2021-09-07 12:18:14 -0700

    Create reset page snackbars.

--- pages/reset.js
+++ pages/reset.js
@@ -1,6 +1,8 @@
 import Background from '../components/Background.js';
 import Router from 'next/router';
 import Link from 'next/link';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
@@ -12,19 +14,36 @@ export default function Reset(props) {
   const { currentUser } = props;
 
   const [email, setEmail] = useState('');
+
   const [error, setError] = useState('');
+  const [isError, setIsError] = useState(false);
   const [success, setSuccess] = useState('');
+  const [isSuccess, setIsSuccess] = useState(false);
 
   // sends password reset email
   async function resetPassword() {
-    setError('');
-    setSuccess('');
+    setIsError(false);
+    setIsSuccess(false);
+    // try to send email
     try {
       await firebase.auth().sendPasswordResetEmail(email);
       setSuccess(`Email sent to ${email}`);
+      setIsSuccess(true);
+    // fail to send email
     } catch (e) {
       setError(getError(e));
+      setIsError(true);
     }
+  }
+
+  // on error closed
+  function onCloseError(event, reason) {
+    if (reason !== 'clickaway') setIsError(false);
+  }
+
+  // on success closed
+  function onCloseSuccess(event, reason) {
+    if (reason !== 'clickaway') setIsSuccess(false);
   }
 
   // listen for user auth
@@ -53,12 +72,38 @@ export default function Reset(props) {
           />
           <button className="bluebutton">Request</button>
         </form>
-        {error && <p>{error}</p>}
-        {success && <p>{success}</p>}
         <hr />
         <Link href="/signin">
           <a>Back to sign in</a>
         </Link>
+        <Snackbar
+          open={isSuccess}
+          autoHideDuration={6000}
+          onClose={onCloseSuccess}
+        >
+          <MuiAlert
+            elevation={6}
+            variant="filled"
+            onClose={onCloseSuccess}
+            severity="success"
+          >
+            {success}
+          </MuiAlert>
+        </Snackbar>
+        <Snackbar
+          open={isError}
+          autoHideDuration={6000}
+          onClose={onCloseError}
+        >
+          <MuiAlert
+            elevation={6}
+            variant="filled"
+            onClose={onCloseError}
+            severity="error"
+          >
+            {error}
+          </MuiAlert>
+        </Snackbar>
       </div>
     </div>
   );
