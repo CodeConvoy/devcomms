fa40848bbc39f06220e2c6e4a6afa5f995e99d28
Cooper Saye <coopersaye@gmail.com>
2021-09-07 18:11:34 -0700

    Implement friend adding.

--- components/channels/Homescreen.js
+++ components/channels/Homescreen.js
@@ -1,5 +1,7 @@
 import Loading from '../Loading.js';
 import Chat from './Chat.js';
+import AddIcon from '@material-ui/icons/Add';
+import Tooltip from '@material-ui/core/Tooltip';
 import Modal from '@material-ui/core/Modal';
 
 import firebase from 'firebase/app';
@@ -17,9 +19,13 @@ export default function Homescreen(props) {
 
   // get user data
   const uid = firebase.auth().currentUser.uid;
-  const usersRef = firebase.firestore().collection('usernames');
+  const usersRef = firebase.firestore().collection('users');
+  const userRef = usersRef.doc(uid);
+
+  // get users data
+  const usernamesRef = firebase.firestore().collection('usernames');
   const chatsRef = firebase.firestore().collection('chats');
-  const [users] = useCollectionData(usersRef);
+  const [users] = useCollectionData(usernamesRef);
 
   // returns message ref based on current user
   function getMessagesRef() {
@@ -27,6 +33,18 @@ export default function Homescreen(props) {
     const chatId = uid < currUser.uid ?
     `${uid}-${currUser.uid}` : `${currUser.uid}-${uid}`;
     return chatsRef.doc(chatId).collection('messages');
+  }
+
+  // adds friend in firebase
+  async function addFriend() {
+    await userRef.update({
+      friends: firebase.firestore.FieldValue.arrayUnion(username)
+    });
+  }
+
+  // resets modal
+  function resetModal() {
+    setUsername('');
   }
 
   // return if loading
@@ -46,6 +64,14 @@ export default function Homescreen(props) {
             </button>
           )
         }
+        <Tooltip title="Add Friend" arrow>
+          <button onClick={() => {
+            resetModal();
+            setModalOpen(true);
+          }}>
+            <AddIcon />
+          </button>
+        </Tooltip>
       </div>
       {
         currUser &&
