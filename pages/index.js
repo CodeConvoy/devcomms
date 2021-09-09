1b0502d03ec4d551c5f27e72900b8a731bf45aae
Cooper Saye <coopersaye@gmail.com>
2021-09-09 14:38:44 -0700

    Create placeholder carousel.

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import AddIcon from '@material-ui/icons/Add';
 import Background from '../components/Background.js';
 import Image from 'next/image';
 import Link from 'next/link';
+import Carousel from 'react-bootstrap/Carousel';
 
 import getIcon from '../util/getIcon.js';
 import firebase from 'firebase/app';
@@ -30,6 +31,43 @@ export default function Index() {
           {getIcon('notes')}
           {getIcon('todos')}
           <AddIcon />
+          <div className={styles.carousel}>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://via.placeholder.com/600x400"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://via.placeholder.com/600x400"
+                  alt="Second slide"
+                />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://via.placeholder.com/600x400"
+                  alt="Third slide"
+                />
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
         </div>
         <p>Instead of having one tool for chatting, one tool for arranging
         notes, and one tool for setting goals, you can do all of that and more
