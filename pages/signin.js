e43956b99a64be90a53e78a9d2b3621b00eed03c
Cooper Saye <coopersaye@gmail.com>
2021-09-08 23:21:57 -0700

    Create user doc.

--- pages/signin.js
+++ pages/signin.js
@@ -5,9 +5,23 @@ import firebase from 'firebase/app';
 import styles from '../styles/pages/SignIn.module.css';
 
 export default function SignIn() {
+  // creates user doc if not existing
+  async function createUserDoc() {
+    // get user doc
+    const uid = firebase.auth().currentUser.uid;
+    const usersRef = firebase.firestore().collection('users');
+    const userDoc = usersRef.doc(uid);
+    // if no user doc exists, create one
+    if (!userDoc.exists) {
+      usersRef.doc(uid).set({ username: uid, friends: [] });
+    }
+  }
+
+  // signs user in with github
   async function signInWithGitHub() {
     const provider = new firebase.auth.GithubAuthProvider();
     await firebase.auth().signInWithPopup(provider);
+    await createUserDoc();
   }
 
   return (
