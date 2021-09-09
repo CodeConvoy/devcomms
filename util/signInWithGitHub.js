6934bcb5c35c3159c3760a5973164ed480d841f9
Cooper Saye <coopersaye@gmail.com>
2021-09-08 23:32:58 -0700

    Extract signInWithGithub function.

---
+++ util/signInWithGitHub.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+
+// creates user doc if not existing
+async function createUserDoc() {
+  // get user doc
+  const uid = firebase.auth().currentUser.uid;
+  const usersRef = firebase.firestore().collection('users');
+  const userDoc = usersRef.doc(uid);
+  // if no user doc exists, create one
+  if (!userDoc.exists) {
+    usersRef.doc(uid).set({ username: uid, friends: [] });
+  }
+}
+
+// signs user in with github
+export default async function signInWithGitHub() {
+  const provider = new firebase.auth.GithubAuthProvider();
+  await firebase.auth().signInWithPopup(provider);
+  await createUserDoc();
+}
