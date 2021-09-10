4467e857688037740d68329e618d3ef31d72c92c
Cooper Saye <coopersaye@gmail.com>
2021-09-10 14:34:19 -0700

    Implement user creation.

--- pages/setup.js
+++ pages/setup.js
@@ -3,6 +3,7 @@ import MuiAlert from '@material-ui/lab/Alert';
 
 import { useEffect, useState } from 'react';
 import firebase from 'firebase/app';
+import getError from '../util/getError.js';
 
 import styles from '../styles/pages/Setup.module.css';
 
@@ -13,6 +14,36 @@ export default function SignUp(props) {
 
   const [error, setError] = useState('');
   const [isError, setIsError] = useState(false);
+
+  // attempts to create user
+  async function createUser() {
+    setIsError(false);
+    // verify username chars
+    if (!/^[A-Za-z0-9_]+$/.test(username)) {
+      setError("Username can only contain alphanumeric characters and underscore.");
+      setIsError(true);
+      return;
+    }
+    // verify username length
+    if (username.length < 2 || username.length > 16) {
+      setError("Username must be between 2 and 16 characters.");
+      setIsError(true);
+      return;
+    }
+    // verify username availability
+    const usernameRef = firebase.firestore().collection('usernames').doc(username);
+    const usernameDoc = await usernameRef.get();
+    if (usernameDoc.exists) {
+      setError("Username is taken. Please try another.");
+      setIsError(true);
+      return;
+    }
+    // create user documents
+    const uid = firebase.auth().currentUser.uid;
+    const userRef = firebase.firestore().collection('users').doc(uid);
+    await userRef.set({ username, friends: [] });
+    await usernameRef.set({ username, uid });
+  }
 
   // on error closed
   function onCloseError(event, reason) {
