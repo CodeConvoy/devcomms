413d04826a7033818bb6fc5003478b6ee9eb4d78
Cooper Saye <coopersaye@gmail.com>
2021-09-10 11:53:26 -0700

    Prompt user for username.

--- util/signInWithGitHub.js
+++ util/signInWithGitHub.js
@@ -2,15 +2,34 @@ import Router from 'next/router';
 
 import firebase from 'firebase/app';
 
+// prompts user for username
+async function getUsername() {
+  const usernamesRef = firebase.firestore().collection('usernames');
+  let username;
+  // prompt user while username invalid
+  while (!username) {
+    username = window.prompt('Enter a username');
+    // verify username existence
+    if (!username) {
+      alert("Please enter a username.");
+    }
+  }
+  return username;
+}
+
 // creates user doc if not existing
 async function createUserDoc() {
   // get user doc
   const uid = firebase.auth().currentUser.uid;
   const usersRef = firebase.firestore().collection('users');
-  const userDoc = usersRef.doc(uid);
+  const usernamesRef = firebase.firestore().collection('usernames');
+  const userRef = usersRef.doc(uid);
+  const userDoc = await userRef.get();
   // if no user doc exists, create one
   if (!userDoc.exists) {
-    usersRef.doc(uid).set({ username: uid, friends: [] });
+    const username = await getUsername();
+    usersRef.doc(uid).set({ username: username, friends: [] });
+    usernamesRef.doc(username).set({ username: username, uid: uid });
   }
 }
 
