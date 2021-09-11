04bfa05df6ea1aa221211b0cb13694fc58a07bd6
Cooper Saye <coopersaye@gmail.com>
2021-09-10 18:58:37 -0700

    Create Modal component.

---
+++ components/Modal.js
@@ -0,0 +1,32 @@
+import BaseModal from '@material-ui/core/Modal';
+import Fade from '@material-ui/core/Fade';
+import Backdrop from '@material-ui/core/Backdrop';
+import ClearIcon from '@material-ui/icons/Clear';
+
+import styles from '../styles/Modal.module.css';
+
+export default function Modal(props) {
+  const { open, onClose } = props;
+
+  return (
+    <BaseModal
+      open={open}
+      onClose={onClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        className: "backdrop",
+        timeout: 100
+      }}
+    >
+      <Fade in={open} timeout={100}>
+        <div className="muimodal">
+          <button className="close" onClick={onClose}>
+            <ClearIcon />
+          </button>
+          {props.children}
+        </div>
+      </Fade>
+    </BaseModal>
+  );
+}
