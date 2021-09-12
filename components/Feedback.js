15c18f894e057e96c9cca72759fe4b2248835fc5
Cooper Saye <coopersaye@gmail.com>
2021-09-11 22:20:10 -0700

    Improve feedback modal styling.

--- components/Feedback.js
+++ components/Feedback.js
@@ -1,7 +1,8 @@
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import AddIcon from '@material-ui/icons/Add';
 import SendIcon from '@material-ui/icons/Send';
-import Modal from '@material-ui/core/Modal';
+import Modal from './Modal';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import { useState } from 'react';
 
@@ -15,54 +16,51 @@ export default function Feedback() {
       <button className={styles.feedbackbtn} onClick={() => setModalOpen(true)}>
         <ChatBubbleIcon />
       </button>
-      <Modal
-        open={modalOpen}
-        onClose={() => setModalOpen(false)}
-      >
-        <div className="muimodal">
-          <h1>Contact Us</h1>
-          <p>Feel free to send us feedback, bug reports, or ideas. We would love
-          to hear from you!</p>
-          <form
-            action="https://formspree.io/f/myyllqwb"
-            method="POST"
-            className={styles.form}
-          >
-            <label>
-              Name<br />
-              <input
-                className="darkinput"
-                type="text"
-                name="name"
-                maxLength="100"
-                required
-              />
-            </label>
-            <label>
-              Email<br />
-              <input
-                className="darkinput"
-                type="email"
-                name="email"
-                maxLength="100"
-                required
-              />
-            </label>
-            <label>
-              Message<br />
-              <textarea
-                className="darkinput"
-                name="message"
-                rows="4"
-                maxLength="10000"
-                required
-              />
-            </label>
+      <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+        <h1>Contact Us</h1>
+        <p>Feel free to send us feedback, bug reports, or ideas. We would love
+        to hear from you!</p>
+        <form
+          action="https://formspree.io/f/myyllqwb"
+          method="POST"
+          className={styles.form}
+        >
+          <label>
+            Name<br />
+            <input
+              className="darkinput"
+              type="text"
+              name="name"
+              maxLength="100"
+              required
+            />
+          </label>
+          <label>
+            Email<br />
+            <input
+              className="darkinput"
+              type="email"
+              name="email"
+              maxLength="100"
+              required
+            />
+          </label>
+          <label>
+            Message<br />
+            <textarea
+              className="darkinput"
+              name="message"
+              rows="4"
+              maxLength="10000"
+              required
+            />
+          </label>
+          <Tooltip title="Send" arrow>
             <button className="iconbutton2" type="submit">
               <SendIcon />
             </button>
-          </form>
-        </div>
+          </Tooltip>
+        </form>
       </Modal>
     </>
   );
