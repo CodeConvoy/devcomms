da47bed91980673a8702f830eca0a06c71cf00d8
Cooper Saye <coopersaye@gmail.com>
2021-09-12 15:30:58 -0700

    Extract User styles.

--- components/channels/User.js
+++ components/channels/User.js
@@ -1,18 +1,24 @@
 import Tooltip from '@material-ui/core/Tooltip';
 import ClearIcon from '@material-ui/icons/Clear';
 
+import styles from '../../styles/components/channels/User.module.css';
+
 export default function User(props) {
-  const { user, currUser, setCurrUser, removeFriend, styles } = props;
+  const { user, currUser, setCurrUser, removeFriend } = props;
 
   return (
     <div
-      className={currUser?.uid === user.uid ? styles.selected : undefined}
+      className={
+        currUser?.uid === user.uid ?
+        `${styles.container} ${styles.selected}` :
+        styles.container
+      }
       onClick={() => setCurrUser(user)}
       key={user.uid}
     >
       <div>{user.username}</div>
       <Tooltip title="Remove Friend" arrow>
-        <button className={styles.removebutton} onClick={e => {
+        <button onClick={e => {
           e.stopPropagation();
           removeFriend(user);
         }}>
